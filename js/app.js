83a1e4f3d0377c93fc456096ef4b99bcd4820253
Toshi <careyeg@gmail.com>
2019-04-30 17:33:11 -0700

    new changes

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var userName = prompt('Greetings what is your name?');
+        var userAge = prompt('How old are you?');
+        var userHobby = prompt('Whats your favorite hobby?');
+        var userDay = prompt('Are you a morning or night person?');
+        alert('Whats up ' + userName + '! I remember when I was '+ userAge + ' and participated in ' + userHobby + ' at ' + userDay + ' lol!');
+        console.log('The user said their name is ' + userName);
+        console.log('The user said their age is ' + userAge);
+        console.log('The user said their hobby is ' + userHobby);
+        console.log('The user said their favorite time of day is ' + userDay);

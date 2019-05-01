3d34a955b8aecd0c69a5a60deb47db312db9a843
Toshi <careyeg@gmail.com>
2019-05-01 10:55:58 -0700

    html, css and js files iz wired

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var userName = prompt('Greetings what is your name?');
+        var userName = prompt('Greetings what is your name?');
         var userAge = prompt('How old are you?');
         var userHobby = prompt('Whats your favorite hobby?');
         var userDay = prompt('Are you a morning or night person?');
@@ -7,3 +7,4 @@ var userName = prompt('Greetings what is your name?');
         console.log('The user said their age is ' + userAge);
         console.log('The user said their hobby is ' + userHobby);
         console.log('The user said their favorite time of day is ' + userDay);
+        

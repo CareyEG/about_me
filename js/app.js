d8acbc46be815a8dedfc520c90fbb6776a3305d7
Toshi <careyeg@gmail.com>
2019-05-01 16:05:21 -0700

    latest and greatest for lab 02

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var userName = prompt('Hi! Welcome! I\'m Carey! What\'s your name?');
         alert('Hi, ' + userName + '! I\'m glad you\'re here! Let\'s play a guessing game so that you can learn more about me.');
         console.log('The user\'s name is' + userName);
 
-var answerOne = prompt('Guess this about me: have I ever in any way shape or form ran over my own finger?').toLowerCase()
+var answerOne = prompt('Guess this about me: have I ever in any way shape or form ran over my own finger?').toLowerCase();
 
 if (answerOne === 'y'  ||  answerOne === 'yes'){
         alert('You\'re right, ' + userName + '! I did! When I used to skateboard, I somehow rolled right over my right index finger! :D');
@@ -13,7 +13,7 @@ if (answerOne === 'y'  ||  answerOne === 'yes'){
         alert('Sorry ' + userName + ', that\'s a big negative!');
         console.log('User got the incorrect answer to #1');
 }
-var answerTwo = prompt('Do I prefer Apple over Android?').toLowerCase()
+var answerTwo = prompt('Do I prefer Apple over Android?').toLowerCase();
 
 if (answerTwo === 'n'  ||  answerTwo === 'no'){
         alert('You\'re right, ' + userName + '! I despise Apple! Android forever!');
@@ -23,7 +23,7 @@ if (answerTwo === 'n'  ||  answerTwo === 'no'){
         console.log('User got the incorrect answer to #2');
 }
 
-var answerThree = prompt('Detroit Lions are my favorite NFL team! But are the Detroit Lions the best team ever?').toLowerCase()
+var answerThree = prompt('Detroit Lions are my favorite NFL team! But are the Detroit Lions the best team ever?').toLowerCase();
 
 if (answerThree === 'n'  ||  answerThree === 'no'){
         alert('Sadly... You\'re right, ' + userName + 'Although I wish it was true...');
@@ -33,7 +33,7 @@ if (answerThree === 'n'  ||  answerThree === 'no'){
         console.log('User got the incorrect answer to #2');
 }
 
-var answerFour = prompt('Do I prefer chocolate over fruity tasting candies?').toLowerCase()
+var answerFour = prompt('Do I prefer chocolate over fruity tasting candies?').toLowerCase();
 
 if (answerFour === 'y'  ||  answerFour === 'yes'){
         alert('You\'re right, ' + userName + '! Chocolate is the best!');
@@ -43,7 +43,7 @@ if (answerFour === 'y'  ||  answerFour === 'yes'){
         console.log('User got the incorrect answer to #4');
 }
         
-var answerFive = prompt('Am I glad this is my last question for this assignment?').toLowerCase()
+var answerFive = prompt('Am I glad this is my last question for this assignment?').toLowerCase();
 
 if (answerFive === 'y'  ||  answerFive === 'yes'){
         alert('You\'re right, ' + userName + '! One small victory but the battle rages on!');
